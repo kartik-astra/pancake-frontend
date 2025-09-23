49ae5a9fb9ad8fa7e1f1dee883ab7eef5ad49b2c
memoyil <2213635+memoyil@users.noreply.github.com>
2025-09-23 17:44:56 +0200

    fix: Build

--- packages/ui-wallets/src/components/SolanaProvider/index.tsx
+++ packages/ui-wallets/src/components/SolanaProvider/index.tsx
@@ -12,7 +12,6 @@ import { ExodusWalletAdapter } from '@solana/wallet-adapter-exodus'
 import { useSetAtom } from 'jotai'
 import { walletConnectConfig } from './walletConnect.config'
 import { errorSolanaAtom } from '../../state/atom'
-import WalletAccountListener from './WalletAccountListener'
 import { TrustAccountsWalletAdapter } from './TrustAccountsWalletAdapter'
 
 export type SolanaProviderProps = React.PropsWithChildren<{
@@ -63,7 +62,6 @@ export const SolanaProvider: React.FC<SolanaProviderProps> = ({ children, endpoi
         onError={onWalletError}
         wallets={walletsAdapter}
       >
-        <WalletAccountListener />
         {children}
       </WalletProvider>
     </ConnectionProvider>
